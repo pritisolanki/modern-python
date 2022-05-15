254fd3b0bd21de61510cac0db99d3b179314788c
pritisolanki <pritiatwork@gmail.com>
2022-05-15 17:50:07 +0530

    Task : Fix language support

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# modern-python
+## modern-python
 
 Exploring setting up a python project with virtual environment.
+
+## Reference
+[Hypermodern Python](https://cjolowicz.github.io/posts/hypermodern-python-01-setup/#installing-python-with-pyenv)
--- src/modern_python/console.py
+++ src/modern_python/console.py
@@ -1,19 +1,16 @@
 import textwrap
-from urllib.error import HTTPError
-
 import click
 import requests
 
 from . import __version__
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
-
 @click.command()
 @click.version_option(version=__version__)
-def main():
-    """The hypermodern Python project."""
+@click.option('--lang',default='en',help="Language preference")
+def main(lang):
+    """The Moden Python project."""
+    API_URL = f'https://{lang}.wikipedia.org/api/rest_v1/page/random/summary'
     try:
         with requests.get(API_URL) as response:
             response.raise_for_status()
