bc9e6a3a5608a904aa616c4daa3de9e753526bda
pritisolanki <pritiatwork@gmail.com>
2022-05-15 17:30:59 +0530

    setting up python project using poetry and click

---
+++ src/modern_python/console.py
@@ -0,0 +1,28 @@
+import textwrap
+from urllib.error import HTTPError
+
+import click
+import requests
+
+from . import __version__
+
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project."""
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            data = response.json()
+
+        title = data["title"]
+        extract = data["extract"]
+
+        click.secho(title, fg="green")
+        click.echo(textwrap.fill(extract))
+    except requests.exceptions.HTTPError as error:
+        print(error)
